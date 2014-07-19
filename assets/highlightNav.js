c6b202a35891ae183b9c787aced0dedb177e9bbd
catherinev <catherinev@users.noreply.github.com>
2014-07-19 02:04:51 -0500

    maybe fixing nav problem on index page

--- assets/highlightNav.js
+++ assets/highlightNav.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
 
 function highlightCurrentPage(){
   var currentUrl = document.URL;
-  console.log(currentUrl);
-  var regex = /http:\/\/(.+)\/([\w\.]+)\??(.*)/;
+
+  var regex = /http:\/\/(.+)\/([\w\.]*)\??(.*)/;
   var baseUrl = currentUrl.replace(regex, "$2")
-  console.log('base')
-  console.log(baseUrl)
+
   $('a[href="/' + baseUrl + '"]').addClass("current")
 }
 ;
