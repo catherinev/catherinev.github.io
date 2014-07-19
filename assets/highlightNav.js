7229744b2755d6c5517a72dc5fb0a4732b5c186e
catherinev <catherinev@users.noreply.github.com>
2014-07-19 14:47:44 -0500

    fix broken links

--- assets/highlightNav.js
+++ assets/highlightNav.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
 
 function highlightCurrentPage(){
   var currentUrl = document.URL;
-  console.log('current: ' + currentUrl)
-  // var regex = /http:\/\/(.+)\/([\w\.]*)\??(.*)/;
+
   var regex = /http:\/\/(.+)\.com\/(.*)/;
   var baseUrl = currentUrl.replace(regex, "$2")
-  console.log('base: ' + baseUrl)
+
   $('a[href="/' + baseUrl + '"]').addClass("current")
 }
 ;
