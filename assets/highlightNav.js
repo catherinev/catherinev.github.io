79b2a996b70ca53c57f64c35d5fbcda4832fe4d0
catherinev <catherinev@users.noreply.github.com>
2014-07-19 21:51:35 -0500

    make regex more specific

--- assets/highlightNav.js
+++ assets/highlightNav.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 function highlightCurrentPage(){
   var currentUrl = document.URL;
 
-  var regex = /http:\/\/(.+)\.com\/(.*)\//;
+  var regex = /http:\/\/(.+)\.com\/(\w*)\//;
   var baseUrl = currentUrl.replace(regex, "$2")
   console.log(baseUrl)
   $('a[href="/' + baseUrl + '/"]').addClass("current_page")
